d63d8929fd1230e228fc4ae0de347e92e83a8431
Anjana Supun <anjanas@wso2.com>
2021-08-12 17:22:37 +0530

    Add utils

---
+++ base-image/execute-tests.sh
@@ -0,0 +1,23 @@
+#!/bin/bash -e
+# Copyright 2021 WSO2 Inc. (http://wso2.org)
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+# ----------------------------------------------------------------------------
+# Running the Load Test
+# ----------------------------------------------------------------------------
+set -e
+sh /base-image/netty-service/netty-start.sh -m 1g -- --ssl --key-store-file /base-image/h1_h1_passthrough/security/ballerinaKeystore.p12 --key-store-password ballerina
+sleep 60s
+cd /base-image/h1_h1_passthrough
+bal run
